3b5cb50012f1b64d5c21d15f457e81fee2beafbd
Mika Dumont <mikakrantman@gmail.com>
2018-02-09 14:32:44 -1000

    added post route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require(`express`);
 const bodyParser = require(`body-parser`);
 
 const userRoutes = require(`./routes/users`);
+const postRoutes = require(`./routes/posts`);
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(`/users`, userRoutes);
+app.use(`/posts`, postRoutes);
 
 // app.get(`/`, (req, res) => {
 //   res.send(`Smoke Test`);
